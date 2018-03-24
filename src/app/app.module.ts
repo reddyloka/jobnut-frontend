8793727812daa8d2934ce199591de7baa98df0d3
Reddyloka <anvesh.reddy@venturepact.com>
2018-03-24 17:16:29 +0530

    singupForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './uni-component/login/login.component';
-import { ApplicantLoginComponent } from './uni-component/applicant-login/applicant-login.component';
-import { HrLoginComponent } from './uni-component/hr-login/hr-login.component';
 import { SignupComponent } from './uni-component/signup/signup.component';
 import { HrComponent } from './uni-component/signup/hr/hr.component';
 import { ApplicantComponent } from './uni-component/signup/applicant/applicant.component';
+import { IndexComponent } from './uni-component/index/index.component';
+import { OtherQualificationComponent } from './uni-component/signup/applicant/other-qualification/other-qualification.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ApplicantLoginComponent,
-    HrLoginComponent,
     SignupComponent,
     HrComponent,
-    ApplicantComponent
+    ApplicantComponent,
+    IndexComponent,
+    OtherQualificationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
