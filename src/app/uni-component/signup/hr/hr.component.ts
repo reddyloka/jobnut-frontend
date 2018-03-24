5ff6da708c72b35e02324dfb35d6cb719a8217fc
Abhishek Mittal <abhishek.mittal@venturepact.com>
2018-03-24 10:24:29 +0530

    qwertyu

--- src/app/uni-component/signup/hr/hr.component.ts
+++ src/app/uni-component/signup/hr/hr.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BattutaService } from '../../../services/battuta.service';
 
 @Component({
   selector: 'app-hr',
@@ -6,10 +7,133 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hr.component.css']
 })
 export class HrComponent implements OnInit {
-
-  constructor() { }
+  BATTUTA_KEY: string;
+  currentCities: any;
+  urlBattuta: string;
+  xmlRequest: any;
+  data: any;
+  countries: any;
+  constructor(private _battutaService: BattutaService) {
+    this.xmlRequest = new XMLHttpRequest();
+    this.BATTUTA_KEY = 'bfb4d38ec36b68f262c513b839c81229';
+    this.currentCities = [];
+    this.countries = {
+    };
+    this.urlBattuta = `https://battuta.medunes.net/api/country/all/?key=00000000000000000000000000000000`;
+  }
 
   ngOnInit() {
+    this._battutaService.getCountries()
+    .subscribe((data) => {
+      console.log('data recievd', data);
+    }, err => console.log('error: ', err));
+
   }
+
+  countryAdded() {
+    // fetch
+  }
+
+
+  //   <script>
+
+  //
+  //       $.getJSON(url,function(countries)
+  //       {
+  //           console.log(countries);
+  //         $('#country').material_select();
+  //           //loop through countries..
+  //           $.each(countries,function(key,country)
+  //           {
+  //               $("<option></option>")
+  //                               .attr("value",country.code)
+  //                               .append(country.name)
+  //                               .appendTo($("#country"));
+
+  //           }); 
+  //           // trigger "change" to fire the #state section update process
+  //           $("#country").material_select('update');
+  //           $("#country").trigger("change");
+
+
+  //       });
+
+  //       $("#country").on("change",function()
+  //       {
+
+  //           countryCode=$("#country").val();
+
+  //           // Populate country select box from battuta API
+  //           url="https://battuta.medunes.net/api/region/"
+  //           +countryCode
+  //           +"/all/?key="+BATTUTA_KEY+"&callback=?";
+
+  //           $.getJSON(url,function(regions)
+  //           {
+  //               $("#region option").remove();
+  //               //loop through regions..
+  //               $.each(regions,function(key,region)
+  //               {
+  //                   $("<option></option>")
+  //                                   .attr("value",region.region)
+  //                                   .append(region.region)
+  //                                   .appendTo($("#region"));
+  //               });
+  //               // trigger "change" to fire the #state section update process
+  //               $("#region").material_select('update');
+  //               $("#region").trigger("change");
+
+  //           }); 
+
+  //       });
+  //       $("#region").on("change",function()
+  //       {
+
+  //           // Populate country select box from battuta API
+  //           countryCode=$("#country").val();
+  //           region=$("#region").val();
+  //           url="https://battuta.medunes.net/api/city/"
+  //           +countryCode
+  //           +"/search/?region="
+  //           +region
+  //           +"&key="
+  //           +BATTUTA_KEY
+  //           +"&callback=?";
+
+  //           $.getJSON(url,function(cities)
+  //           {
+  //               currentCities=cities;
+  //               var i=0;
+  //               $("#city option").remove();
+
+  //               //loop through regions..
+  //               $.each(cities,function(key,city)
+  //               {
+  //                   $("<option></option>")
+  //                                   .attr("value",i++)
+  //                                   .append(city.city)
+  //                           .appendTo($("#city"));
+  //               });
+  //               // trigger "change" to fire the #state section update process
+  //               $("#city").material_select('update');
+  //               $("#city").trigger("change");
+  //           }); 
+  //       }); 
+  //       $("#city").on("change",function()
+  //       {
+  //         currentIndex=$("#city").val();
+  //         currentCity=currentCities[currentIndex];
+  //         city=currentCity.city;
+  //         region=currentCity.region;
+  //         country=currentCity.country;
+  //         lat=currentCity.latitude;
+  //         lng=currentCity.longitude;
+  //         $("#location").html('<i class="fa fa-map-marker"></i> <strong> '+city+"/"+region+"</strong>("+lat+","+lng+")");
+  //       });
+  // </script>
+
+
+
+
 
 }
--- src/app/uni-component/signup/signup.component.ts
+++ src/app/uni-component/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  hrActive: boolean;
+  applicantActive: boolean;
+  signupActive: boolean;
+  userActiveClasses: string[];
+  signUpHead: any;
+  constructor(private router: Router,
+    private location: Location) {
+    this.userActiveClasses = [];
+    this.signupActive = true;
+    this.signUpHead = {
+      'fadeOutDownBig': false,
+      'animated': true
+    };
+    this.applicantActive = false;
+    this.hrActive = false;
+  }
 
   ngOnInit() {
+  }
+
+  public goBackButton() {
+    this.location.back();
+  }
+
+  frontViewChange(sender, cb) {
+    if (this.signupActive) {
+      return false;
+    }
+    this.signUpHead['fadeOutDownBig'] = true;
+    this.userActiveClasses.push(
+      'fadeOut',
+      'animated'
+    );
+    cb(true);
+  }
+
+  signupComponentCall(caller: string) {
+    this.signupActive = !this.signupActive;
+    this.frontViewChange(caller, (res) => {
+      if (res) {
+        if (caller === 'hr') {
+          this.hrActive = true;
+        } else if (caller === 'applicant') {
+          this.applicantActive = true;
+        }
+      }
+    });
   }
 
 }
