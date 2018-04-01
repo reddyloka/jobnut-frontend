bb9a04fc8edd27f9f6486af68fc3dd7593f04d70
Shubham Latiyan <shubham.latiyan@venturepact.com>
2018-04-01 22:34:13 +0530

    commiting

--- src/app/model/hrpostdetails.ts
+++ src/app/model/hrpostdetails.ts
@@ -1,23 +1,23 @@
 export class HrPostDetail {
     
-    public id: number;
-    public title: string;
-    public companyname: string;
-    public description: string;
-    public startdate: Date;
-    public enddate: Date;
-    public skills: string;
-    public location: string;
-    public salary: number;
-    public experinece: string;
-    public dateOfJoining: Date;
-    public extraRequirement: string;
-    public noOfJobOpenings: number;
-    public CompanyUrl: string;
-    public bondDetails: any;
-    public ReportingVenue: any;
-    public ResourcePersonContact: any;
-    public selectionProcedure: string;
+    id: number;
+    title: string;
+    companyname: string;
+    description: string;
+    startdate: Date;
+    enddate: Date;
+    skills: string;
+    location: string;
+    salary: number;
+    experinece: string;
+    dateOfJoining: Date;
+    extraRequirement: string;
+    noOfJobOpenings: number;
+    CompanyUrl: string;
+    bondDetails: any;
+    ReportingVenue: any;
+    ResourcePersonContact: any;
+    selectionProcedure: string;
     constructor(id: number, title: string, companyname: string, description: string, startdate: Date, enddate: Date, skills: string, location: string , salary: number, experinece: string, dateOfJoining: Date, extraRequirement: string, noOfJobOpenings: number, CompanyUrl: string, bondDetails: any, ReportingVenue: any, ResourcePersonContact: any, selectionProcedure: string) {
         this.id = id;
         this.title = title;
