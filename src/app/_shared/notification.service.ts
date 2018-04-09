020c0ac4a19c55a7ef9266dbcb5f058052912ae4
Abhishek Mittal <abhishek.mittal@venturepact.com>
2018-04-09 09:43:46 +0530

    69

--- src/app/_shared/notification.service.ts
+++ src/app/_shared/notification.service.ts
@@ -7,16 +7,21 @@ export class NotificationService {
   title: string;
   stay: number;
 
+  activatePopup: boolean;
+
   constructor() {
     this.message = 'nothing to display';
     this.title = 'Notifiation';
-    this.stay = 2500;
+    this.stay = 3000;
+    this.activatePopup = false;
    }
 
    pop(message: string, title: string, stay: number ) {
+     console.log('popped');
     this.message = message;
     this.title = title;
     this.stay = stay;
+    this.activatePopup = true;
    }
 
 }
--- src/app/uni-component/misc/notification/notification.component.ts
+++ src/app/uni-component/misc/notification/notification.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, HostBinding, Directive, HostListener, Input, OnChanges, SimpleChange, EventEmitter } from '@angular/core';
 import { NotificationService } from '../../../_shared/notification.service';
 
+// @Directive({
+//   // tslint:disable-next-line:directive-selector
+//   selector: '[popup]'
+// })
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html'
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent implements OnInit, OnChanges {
 
+  @HostBinding('attr.class') cssClass = 'hidden';
+  @Output()
+  donePopup = new EventEmitter<boolean>();
+  @Input() data: any;
   message: string;
   title: string;
+  isActive: boolean;
   stay: number;
-  constructor(
-    private _notificationService: NotificationService
-  ) {
-    this.message =  _notificationService.message;
-    this.title = _notificationService.title;
-    this.stay = _notificationService.stay;
-   }
+
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    if (this.data) {
+      Object.assign(this, this.data);
+      this.cssClass = this.isActive ? 'active' : 'hidden';
+    }
+    console.log(this);
+  }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.donePopup.emit(false);
+    }, this.stay);
+  }
+
+  @HostListener('click') displayMessage(): void {
+    this.donePopup.emit(false);
   }
 
 }
