51b224be8c639010cb8c8854664eb408f0e276a1
reddyloka <anvesh.reddy@venturepact.com>
2018-12-10 12:23:11 +0530

    ase

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/jobnut'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname, 'build', 'index.html'));
+res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port
