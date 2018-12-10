b9937e62dcf2025b72c0e0c81290544c404e4cb3
reddyloka <anvesh.reddy@venturepact.com>
2018-12-10 13:01:42 +0530

    lolli

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // server.js
 const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
 const app = express();
  
 // If an incoming request uses
@@ -22,19 +20,20 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-//app.use(forceSSL());
+app.use(forceSSL());
  
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-/*
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-*/
+// app.use(express.static(__dirname + '/dist'));
+
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
  
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 4200, function() {
